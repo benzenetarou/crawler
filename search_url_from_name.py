c9684c3cf027bd6c9378516400a1cfa6c876c464
benzenetarou <benzenetarou@gmail.com>
2017-06-29 14:25:31 +0900

    fix typo and comments

--- search_url_from_name.py
+++ search_url_from_name.py
@@ -1,4 +1,4 @@
-# csvファイルから1行つづurlを読み取ってアクセスし、ページから所在地情報のタグを抜き出すプログラムです。
+#csvファイルの一行目の文字列をbingで検索した結果の一位のサイトのドメインを出力するプログラム。
 
 from bs4 import BeautifulSoup
 import urllib.request
@@ -9,11 +9,9 @@ from urllib.parse import urlparse
 csv_reader = csv.reader(open("name.csv", "r"), delimiter=",", quotechar='"')
 for row in csv_reader:
 
-# ココらへんはお決まりのBeautifulSoupの流れ〜
     search_name_japanese = row[0]
-    url = str('https://www.bing.com/search?q=' + company_name_japanese + '&qs=n&form=QBLH&pq=' + company_name_japanese + '&sc=1-0&sp=-1&sk=&cvid=26AD2F8AFF204F3FA5060839422F1001')
+    url = str('https://www.bing.com/search?q=' + search_name_japanese + '&qs=n&form=QBLH&pq=' + search_name_japanese + '&sc=1-0&sp=-1&sk=&cvid=26AD2F8AFF204F3FA5060839422F1001')
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-
 
     p = urlparse(url)
     query = urllib.parse.quote_plus(p.query, safe='=&')
@@ -23,16 +21,15 @@ for row in csv_reader:
     '?' if p.query else '', query,
     '#' if p.fragment else '', p.fragment)
     response = urllib.request.urlopen(url)
-    # print(url)
-    # response = urlopen(req)
+    # ココらへんはお決まりのBeautifulSoupの流れ〜
     html = response.read()
     soup = BeautifulSoup(html, "lxml")
     url_class = soup.find(class_ = 'b_algo')
-    if url_class is None:
+    if url_class is None: #なぜかうまくいかないときもあるので、回避策としてfailureと出力
         print("failure")
     else:
         url_a = url_class.find('a')
-    # 以下で正規化する
+    # 以下でサブディレクトリを切り捨てる
         url = str(url_a['href'])
         if(url[:url.find('/',8)]==' '):
             print(url+"/")
